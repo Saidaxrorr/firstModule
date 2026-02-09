f0f1438aec266e4c3c13422e1c25b61fdfca7058
axror <notmasterpoo1@gmail.com>
2026-02-09 22:09:03 +0500

    Student class added

---
+++ 1.8 dars (Constructor, CRUD)/1.8 dars (Constructor, CRUD)/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace _1._8_dars__Constructor__CRUD_;
+
+public class Student
+{
+    public string FirstName {  get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
