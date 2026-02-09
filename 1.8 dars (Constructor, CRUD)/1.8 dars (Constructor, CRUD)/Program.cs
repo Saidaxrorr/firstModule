82b54ffb590e79bcb070888949743e59a70d2a57
axror <notmasterpoo1@gmail.com>
2026-02-09 22:00:22 +0500

    Constructor CRUD

---
+++ 1.8 dars (Constructor, CRUD)/1.8 dars (Constructor, CRUD)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1._8_dars__Constructor__CRUD_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
